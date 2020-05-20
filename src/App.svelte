6ee0b6f0b4590ead9770291ee689b435b9861410
jwlarocque <jwlarocque@gmail.com>
2020-05-20 16:53:24 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    import ItemComponent from "./ItemComponent.svelte";
+
+    let data = ["Apples", "Butter"];
+</script>
+
+<main>
+
+	<ItemComponent bind:data={data}/>
+</main>
+
---
+++ src/ItemComponent.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let data = [];
+
+    function swap() {
+        let temp = data[0];
+        data[0] = data[1];
+        data[1] = temp;
+        data = data;
+        console.log("swapped")
+    }
+</script>
+
+<main>
+    <div class="list">
+        <button on:click={swap}>Swap Items</button>
+        {#each data as datum (datum)}
+            <div on:click|stopPropagation={function(ev) {console.log("mousedown!");}}>
+                <p>{datum}</p>
+            </div>
+        {/each}
+    </div>
+</main>
